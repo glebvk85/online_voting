e75a7486905542e6137b58dd813e812a64c0c60a
gleb.kucherenko@freemake.com <gleb.kucherenko@freemake.com>
2017-12-20 18:35:31 +0300

    public start

--- OnlineVoting/OnlineVoting/runserver.py
+++ OnlineVoting/OnlineVoting/runserver.py
@@ -1,8 +1,7 @@
-
 from os import environ
 from OnlineVoting import app
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     PORT = 11234
-    app.run(HOST, PORT)
+    app.run(host='192.168.0.121', port=PORT, threaded=True)
