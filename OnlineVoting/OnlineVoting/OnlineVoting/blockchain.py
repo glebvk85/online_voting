bde6b2319ef70907e75ca6964e4f512bda305ce7
gleb.kucherenko@freemake.com <gleb.kucherenko@freemake.com>
2018-01-30 23:28:14 +0300

    set november

--- OnlineVoting/OnlineVoting/OnlineVoting/blockchain.py
+++ OnlineVoting/OnlineVoting/OnlineVoting/blockchain.py
@@ -360,7 +360,7 @@ class DataBaseSystem:
             voting = self.get_voting_by_member_and_lecture(i[0], i[1])
             if voting is None:
                 contract = Vote(member.id, member.username, lecture.id)
-                contract.timestamp = int(time.mktime(date.datetime.strptime('09-10-2017 07:42:00', '%d-%m-%Y %H:%M:%S').timetuple()))
+                contract.timestamp = int(time.mktime(date.datetime.strptime('09-11-2017 07:42:00', '%d-%m-%Y %H:%M:%S').timetuple()))
                 appendTransaction(contract)
                 self.transactions.append(contract)
 
