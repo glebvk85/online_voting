1de6cbc04315717c2ca077d1c7d340e1fef0ecdc
gleb.kucherenko@freemake.com <gleb.kucherenko@freemake.com>
2018-01-31 17:12:44 +0300

    set feedback

--- OnlineVoting/OnlineVoting/runserver.py
+++ OnlineVoting/OnlineVoting/runserver.py
@@ -3,11 +3,12 @@ from OnlineVoting import app
 import argparse
 import logging
 
-def configure_log(config):
+def configure_log(log_filename):
     logging.basicConfig(filename=log_filename, level=logging.DEBUG, format='[%(asctime)s] %(levelname).1s %(message)s',
                         datefmt='%Y.%m.%d %H:%M:%S')
 
 if __name__ == '__main__':
+    configure_log('errors.log')
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', default='127.0.0.1')
 
