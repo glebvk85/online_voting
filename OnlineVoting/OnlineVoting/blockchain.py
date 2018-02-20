740a009c31f00d367623f403d415ce1f1b108504
gleb.kucherenko@freemake.com <gleb.kucherenko@freemake.com>
2018-02-20 10:13:15 +0300

    remove migration

--- OnlineVoting/OnlineVoting/blockchain.py
+++ OnlineVoting/OnlineVoting/blockchain.py
@@ -263,15 +263,6 @@ class DataBaseSystem:
                                 return (item['idMemberCreator'], speakers)
 
     def sync_lectures(self):
-        for item in self.transactions:
-            if item.type == 'Contract':
-                if item.hash_contract == 'd1ec5b73915807a3aac8903233bfcbfaaecadf09e3446c68f08a797f189e54a0' or item.hash_contract == 'ce0f16b046cedc269e50ad39540eade7397a10f113d2c4cd2de96d896d264581':
-                    item.hash_contract = get_hash_contract('theme')
-                    write_transaction(item)
-                if item.hash_contract == '90e8255cf28c2979b69f2a91439ebd1b765f46884de2ef7d4653e71af6c6b060':
-                    item.hash_contract = get_hash_contract('publication')
-                    write_transaction(item)
-        return
         # sync new lectures
         for item in self.allCards:
             contract = self.get_lecture_contract(item.id)
